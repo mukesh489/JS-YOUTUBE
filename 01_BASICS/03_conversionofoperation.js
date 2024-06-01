f2e80659a64635ec2309971bc3faa4dbd4acf605
mukesh489 <83478699+mukesh489@users.noreply.github.com>
2024-06-01 21:22:19 +0000

    COMMIT ALL

---
+++ 01_BASICS/03_conversionofoperation.js
@@ -0,0 +1,20 @@
+let score ="33"
+
+//console.log(typeof score)
+
+let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+// 1 is true
+//0 is false
+//"" string is false 
+let someNumber =33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+// ************************************************OPERATION*********************************//
+
